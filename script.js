f2ab3a26bc7174ed13f6c47014f77b93ca78f41c
Pawan Pandey <pandeypawan686800@gmail.com>
2025-08-26 20:37:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+let input=document.getElementById("input");
+let msg= document.getElementById("msg");
+  let attemp = document.getElementById("attemt");
+let buttons=document.getElementById("buttons")
+
+
+    attemp=[]
+    for(i=0;i<8;i++){
+        attemp.push(i);
+    }
+
+function display(value){
+    input.value+=value;
+}
+
+function erase(){
+    input.value=input.value.slice(0,-1)
+}
+
+let value = document.getElementById("random");
+value.innerText=Math.floor(Math.random()*100)
+
+
+function cheak(){
+
+    if(input.value.length==0){
+        alert("Please Enter Some Value ")
+    }
+  else{
+    i=attemp.pop()
+    document.getElementById("attempt")
+    attempt.innerText="Attempt Remaining :"+i;
+   if(i==0){
+    alert("You Lost ! Better Luck Next Time")
+    location.reload()
+    }                                                               
+  }
+
+
+    let guess = Number(input.value);
+let target = Number(value.innerText);
+
+if (guess === target) {
+    msg.innerText = "Correct Answer!";
+    value.style.color = "white"; 
+
+    document.querySelectorAll("button").forEach(btn => {
+        if (btn.id !== "reset") {
+            btn.disabled = true;
+        }
+    });
+}
+
+else if (guess < 0 || guess > 100) {
+    msg.innerText = "Number Not Allowed!";
+    input.value="";
+}
+else if (guess < target) {
+    msg.innerText = "Smaller than random value";
+    input.value="";
+}
+else if (guess > target) {
+    msg.innerText = "Greater than random value";
+    input.value="";
+}
+
+        
+    }
+
+   
+
+function reset(){
+    location.reload();
+}
+
+                
